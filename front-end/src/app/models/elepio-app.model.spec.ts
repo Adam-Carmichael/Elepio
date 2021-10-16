a183fbf0bb8d1d65d8108318e3f1d9cbcf8ad411
alc73 <alc73@students.uwf.edu>
2021-10-16 18:37:15 -0400

    Added requirement_file.txt for managing dependencies

--- front-end/src/app/models/elepio-app.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ElepioApp } from './elepio-app.model';
-
-describe('ElepioApp', () => {
-  it('should create an instance', () => {
-    expect(new ElepioApp()).toBeTruthy();
-  });
-});
--- front-end/src/app/models/elepio-app.model.ts
+++
@@ -1,6 +0,0 @@
-export class ElepioApp {
-    id?: any;
-    title?: string;
-    description?: string;
-    published?: boolean;
-}

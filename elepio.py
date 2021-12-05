1a07f6ed18ebf6343f2bbcf25f58c050f3745a3e
alc73 <alc73@students.uwf.edu>
2021-12-04 21:10:46 -0500

    endpoint getBoards is not working, but app runs

--- elepio.py
+++ elepio.py
@@ -12,33 +12,14 @@ app = Flask(__name__)
 app.config.from_pyfile('config.cfg')
 db = MongoEngine(app)
 
-# Example of saving values to a document
-"""
-bttf = Movie(title="Back To The Future", year=1985)
-bttf.actors = [
-    "Michael J. Fox",
-    "Christopher Lloyd"
-]
-bttf.imdb = Imdb(imdb_id="tt0088763", rating=8.5)
-bttf.save()
-"""
-
-# Example of querying a document
-"""
-bttf = Movies.objects(title="Back To The Future").get_or_404()
-"""
-
-# Example of concatenated query whose response you can iterate over
-"""
-some_theron_movie = Movie.objects(actors__in=["Charlize Theron"]).first()
-
-for recents in Movie.objects(year__gte=2017):
-    print(recents.title)
-"""
-
 @app.route('/') # the route decorator which specifies what URL triggers the following function(s)
-def hello_world():
-    return "<p>Hello World!</p>"
+def elepio_doc():
+    return "<p>Welcome to the placeholder for the Elepio documentation</p>"
+
+@app.route('/api/getBoards', methods=['GET'])
+def get_boards():
+    boards = Board.objects()
+    return boards.to_json()
 
 """@app.route('/api/getFirstActiveBoard', methods=['GET', 'POST']) # if GET, you have HEAD and OPTIONS supported by Flask
 def getFirstActiveBoard():

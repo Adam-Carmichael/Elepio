4b6129dd1eb74bd89184af283631c33026487a5f
alc73 <alc73@students.uwf.edu>
2022-10-22 19:07:17 -0400

    added api/boards GET route, started api/boards POST route. Need to test connectivity still

--- app.py
+++ app.py
@@ -11,10 +11,48 @@ app = Flask(__name__)
 def get_db_connection():
     conn = psycopg2.connect(
         host="localhost",
-        database="flask_db",
+        database="elepio",
         user=os.environ['DB_USERNAME'],
         password=os.environ['DB_PASSWORD'])
     
     return conn
 
+#####################################
+###
+###  The Board's CRUD operations  ###
+###
+#####################################
 
+# Returns all boards
+@app.route('/api/boards', methods=['GET'])
+def get_boards():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM board;')
+    boards = cur.fetchall()
+    cur.close()
+    conn.close()
+
+    if len(boards) == 0:
+        return boards, 404
+
+    return boards, 200
+"""
+# Create/update a board
+@app.route('/api/boards', methods=['POST', 'PATCH'])
+@cross_origin()
+def create_board():
+    try:
+        data = request.get_json()
+        board = Board(**data)
+        board.save()
+    except ValidationError:
+        response = format_response({"message": "Validation error thrown, please check your posted body for errors"})
+        return response, 400
+    except FieldDoesNotExist:
+        response = format_response({"message": "Field does not exist error thrown, please check your posted body for errors"})
+        return response, 400
+    
+    response = format_response(board)
+    return response, 200
+"""
